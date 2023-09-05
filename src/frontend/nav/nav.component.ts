06b9cdfb1125c0c2a24535cec656a48a858d5cd0
noutcheu libert <joran.noutcheu@facsciences-uy1.cm>
2023-09-05 01:52:13 +0100

    adding of body card and start js filtring

--- src/frontend/nav/nav.component.ts
+++ src/frontend/nav/nav.component.ts
@@ -8,17 +8,21 @@ import { Component, OnInit } from '@angular/core';
 export class NavComponent {
   bodyElement = document.body;
   changetheme:boolean = false;
-
+  theme:string = "../../assets/night.svg"
+  nameMode:string = "Dark Mode";
   changeTheme(): void{
     
     if (this.changetheme) {
         document.documentElement.removeAttribute("data-theme");
         this.changetheme  = false;
+        this.nameMode = "Dark Mode";
     } else {
       document.documentElement.setAttribute("data-theme", "dark");
       this.changetheme = true;
+      this.nameMode = "Light Mode";
     }
     
+    this.theme = this.changetheme?"../../assets/sun.svg":"../../assets/night.svg";
     
   }
 
